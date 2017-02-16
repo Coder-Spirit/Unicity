49c361f52b92c8d76c8c4d547d6bde0858ab2bec
Andreu Correa Casablanca <castarco@gmail.com>
2017-02-16 08:18:55 +0100

    Increase testing coverage

--- tests/GUID/fromHexStringTest.php
+++ tests/GUID/fromHexStringTest.php
@@ -13,6 +13,24 @@ use Unicity\GUID;
 class fromHexStringTestTest extends TestCase
 {
     /**
+     * @expectedException \Unicity\Errors\GUIDInvariantsViolationError
+     * @expectedExceptionMessage IDs must have at least 6 bytes of entropy
+     */
+    public function test_too_small_expected_length()
+    {
+        GUID::fromHexString('abcd', 4);
+    }
+
+    /**
+     * @expectedException \Unicity\Errors\UnserializationError
+     * @expectedExceptionMessage The passed string has an unexpected length {"expected":6,"given":4}
+     */
+    public function test_unexpected_length()
+    {
+        GUID::fromHexString('abcdef01', 6);
+    }
+
+    /**
      * @param string $invalidHexStr
      *
      * @dataProvider invalidHexStringsProvider
@@ -21,7 +39,7 @@ class fromHexStringTestTest extends TestCase
      */
     public function test_invalid_hex_strings(string $invalidHexStr)
     {
-        GUID::fromHexString($invalidHexStr, \strlen($invalidHexStr)/2);
+        GUID::fromHexString($invalidHexStr, (int)(\strlen($invalidHexStr)/2));
     }
 
     public function invalidHexStringsProvider()
@@ -29,7 +47,8 @@ class fromHexStringTestTest extends TestCase
         return [
             ['xyxyxyxyxyxy'],
             ['abcdefABCDEF'],
-            ['0123456789a-']
+            ['0123456789a-'],
+            ['0123456789abc']
         ];
     }
 }
